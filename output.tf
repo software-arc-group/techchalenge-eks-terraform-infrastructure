f4bc51ee24d92847875b21df736347cc94b8023c
Washington Oliveira <w_olv@hotmail.com>
2024-11-08 08:30:46 -0300

    versao inicial ja criando cluster no EKS

--- output.tf
+++ output.tf
@@ -9,7 +9,3 @@ output "cluster_endpoint" {
 output "cluster_security_group_id" {
   value = module.eks.cluster_security_group_id
 }
-
-output "node_group_role_arn" {
-  value = module.eks.node_groups["eks_nodes"].iam_role_arn
-}
--- variables.tf
+++ variables.tf
@@ -5,13 +5,37 @@ variable "region" {
 }
 
 variable "cluster_name" {
-  description = "Nome do cluster EKS"
+  description = "techchallenge"
   type        = string
-  default     = "my-eks-cluster"
+  default     = "techchallenge-eks-cluster"
+}
+
+variable "node_instance_type" {
+  description = "Tipo de instância para os nós do EKS"
+  type        = string
+  default     = "t3.medium"
 }
 
 variable "node_group_size" {
   description = "Tamanho do grupo de nós EKS"
   type        = number
+  default     = 1
+}
+
+variable "desired_capacity" {
+  description = "Capacidade desejada de nós no cluster"
+  type        = number
   default     = 2
+}
+
+variable "max_size" {
+  description = "Capacidade máxima de nós no cluster"
+  type        = number
+  default     = 3
+}
+
+variable "min_size" {
+  description = "Capacidade mínima de nós no cluster"
+  type        = number
+  default     = 1
 }
