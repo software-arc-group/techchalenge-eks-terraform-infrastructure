0c073f19aa1dfec2e1d0aeabad8f9425f5c7fc81
Washington Oliveira <w_olv@hotmail.com>
2024-11-25 20:12:42 -0300

    adiciona ecr

--- output.tf
+++ output.tf
@@ -9,3 +9,8 @@ output "cluster_endpoint" {
 output "cluster_security_group_id" {
   value = module.eks.cluster_security_group_id
 }
+
+output "ecr_repository_url" {
+  value       = aws_ecr_repository.tech-challenge.repository_url
+  description = "URL do repositório ECR para o app"
+}
