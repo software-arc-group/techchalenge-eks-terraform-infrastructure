20f507599d17b6ee6f2ecacc98c1451d775144a9
Washington Oliveira <w_olv@hotmail.com>
2024-12-14 11:14:05 -0300

    cria banco de dados publicamente acessivel

--- aws-apigateway.tf
+++ aws-apigateway.tf
@@ -1,36 +1,36 @@
-# locals {
-#   openapi_file = templatefile("./openapi/apigateway.tfpl", {
-#       server_url = module.eks.cluster_endpoint, 
-#       cognito_arn = aws_cognito_user_pool.pool.arn, 
-#       lambda_uri= data.aws_lambda_function.valida_credenciais.invoke_arn, 
-#       iam_arn=aws_iam_role.gateway_role.arn 
-#     }
-#   )
-# }
+locals {
+  openapi_file = templatefile("./openapi/apigateway.tfpl", {
+      server_url = module.eks.cluster_endpoint, 
+      cognito_arn = aws_cognito_user_pool.pool.arn, 
+      lambda_uri= data.aws_lambda_function.valida_credenciais.invoke_arn, 
+      iam_arn=aws_iam_role.gateway_role.arn 
+    }
+  )
+}
 
-# data "aws_lambda_function" "valida_credenciais" {
-#   function_name = var.lambda_name
-# }
+data "aws_lambda_function" "valida_credenciais" {
+  function_name = var.lambda_name
+}
 
-# resource "aws_api_gateway_rest_api" "apigateway_lanchonete" {
-#   body = local.openapi_file
-#   name = "apigatway-lanchonete"
-# }
+resource "aws_api_gateway_rest_api" "apigateway_lanchonete" {
+  body = local.openapi_file
+  name = "apigatway-lanchonete"
+}
 
-# resource "aws_api_gateway_deployment" "deployment" {
-#   rest_api_id = aws_api_gateway_rest_api.apigateway_lanchonete.id
+resource "aws_api_gateway_deployment" "deployment" {
+  rest_api_id = aws_api_gateway_rest_api.apigateway_lanchonete.id
 
-#   triggers = {
-#     redeployment = sha1(local.openapi_file)
-#   }
+  triggers = {
+    redeployment = sha1(local.openapi_file)
+  }
 
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-# resource "aws_api_gateway_stage" "prod" {
-#   deployment_id = aws_api_gateway_deployment.deployment.id
-#   rest_api_id   = aws_api_gateway_rest_api.apigateway_lanchonete.id
-#   stage_name    = "prod"
-# }
+resource "aws_api_gateway_stage" "prod" {
+  deployment_id = aws_api_gateway_deployment.deployment.id
+  rest_api_id   = aws_api_gateway_rest_api.apigateway_lanchonete.id
+  stage_name    = "prod"
+}
